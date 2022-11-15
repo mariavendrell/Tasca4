af98072750b1583da3b6a0d59f36ce7cc736fc90
mariavendrell <mvendrellgarcia.cf@iesesteveterradas.cat>
2022-11-15 17:58:51 +0100

    v1

---
+++ Correu365.ps1
@@ -0,0 +1,27 @@
+<#  Intenta realitzar la tasca amb el correu de Office365. El correu pot ser el de iesesteveterradas.cat, 
+    ja que us aporta accés a Office 365. 
+    https://techexpert.tips/es/powershell-es/powershell-enviar-correo-electronico-con-office-365/   #>
+$Backup=$args[0]
+$RutaBackup=$args[1]
+    
+if ($Backup -eq $null -And $RutaBackup -eq $null){
+    Write-Output "Els parametres que m'has donat no son valids"
+}
+else 
+{
+    #Comprovar que els arguments siguin valits
+    $existeix1= Test-Path $Backup
+    $existeix2= Test-Path $RutaBackup
+    
+    if ($existeix1 -And $existeix2 -eq $True)
+    {
+        Copy-Item -Path $Backup -Destination $RutaBackup -Recurse -Force
+        $mycredentials = Get-Credential
+        Send-MailMessage -SmtpServer smtp.office365.com -Port 587 -UseSsl -From mvendrellg@office365.com -To mvendrellgarcia.cf@iesesteveterradas.cat -Subject 'Backup feta' -Body 'Backup feta' -Credential $mycredentials
+        Write-Output "Copia de seguretat realitzada"
+    }
+    else
+    {
+        Write-Output "Els parametres que m'has donat no son valids"
+    }
+}
---
+++ CorreuBackup.ps1
@@ -0,0 +1,28 @@
+<#  Genera un script que permeti enviar un correu per gmail quan finalitzi la còpia d'uns fitxers. Pots reutilitzar el codi que 
+    vas utilitzar per fer les còpies de segureta per tal que al finalitzar enviï un correu de confirmació.                     
+    https://techexpert.tips/es/powershell-es/powershell-enviar-correo-electronico-con-gmail/     #>
+
+$Backup=$args[0]
+$RutaBackup=$args[1]
+    
+if ($Backup -eq $null -And $RutaBackup -eq $null){
+    Write-Output "Els parametres que m'has donat no son valids"
+}
+else 
+{
+    #Comprovar que els arguments siguin valits
+    $existeix1= Test-Path $Backup
+    $existeix2= Test-Path $RutaBackup
+    
+    if ($existeix1 -And $existeix2 -eq $True)
+    {
+        Copy-Item -Path $Backup -Destination $RutaBackup -Recurse -Force
+        $mycredentials = Get-Credential
+        Send-MailMessage -SmtpServer smtp.gmail.com -Port 587 -UseSsl -From mariavendrell2003@gmail.com -To mvendrellgarcia.cf@iesesteveterradas.cat -Subject 'Backup feta' -Body 'Backup feta' -Credential $mycredentials
+        Write-Output "Copia de seguretat realitzada"
+    }
+    else
+    {
+        Write-Output "Els parametres que m'has donat no son valids"
+    }
+}
